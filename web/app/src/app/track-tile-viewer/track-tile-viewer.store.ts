cc068a4df3b28a0acccbe9bba26e3df81a609769
Fabian Feitsch <info@fafeitsch.de>
2021-07-22 21:39:32 +0200

    docu: add preview image

--- web/app/src/app/track-tile-viewer/track-tile-viewer.store.ts
+++ web/app/src/app/track-tile-viewer/track-tile-viewer.store.ts
@@ -5,23 +5,23 @@ import {pluck, tap} from 'rxjs/operators';
 import {Observable} from 'rxjs';
 
 interface State {
-  seed: string;
+  defaultSeed: string;
   version: string;
   buildTime: string;
 }
 
 @Injectable()
 export class TrackTileViewerStore extends ComponentStore<State> {
-  readonly seed$ = super.select(state => state.seed);
+  readonly seed$ = super.select(state => state.defaultSeed);
   readonly version$ = super.select(state => state.version);
   readonly buildTime$ = super.select(state => state.buildTime);
 
-  readonly setSeed$ = super.updater((state, seed: string) => ({...state, seed}))
+  readonly setSeed$ = super.updater((state, seed: string) => ({...state, defaultSeed: seed}))
 
   readonly loadConfig = super.effect((trigger$: Observable<void>) => this.service.fetchConfig()
     .pipe(tap(config => super.patchState(config))))
 
   constructor(private readonly service: ConfigService) {
-    super({seed: '', buildTime: '', version: ''});
+    super({defaultSeed: '', buildTime: '', version: ''});
   }
 }
